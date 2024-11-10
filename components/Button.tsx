c5990b882609b7a2d043acb359373ad34bb27dee
oskygon <oskygon@yahoo.es>
2024-11-10 12:00:22 -0300

    Agregamos cambio de imagenes

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,10 +4,11 @@ import { StyleSheet, View, Pressable, Text } from 'react-native';
 type Props = {
   label: string;
   theme?: 'primary';
+  onPress?: () => void;
 };
 
-export default function Button({ label, /* @tutinfo The prop <CODEtheme</CODE> to detect the button variant. */theme }: Props) {
-  /* @tutinfo Conditionally render the primary themed button. */
+export default function Button({ label, theme, onPress }: Props) {
+ 
   if (theme === 'primary') {
     return (
       <View
@@ -16,14 +17,14 @@ export default function Button({ label, /* @tutinfo The prop <CODEtheme</CODE> t
           { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
         ]}>
         <Pressable
-          style={[styles.button, { backgroundColor: '#fff' }]}
-          onPress={() => alert('You pressed a button.')}>
+          style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
+     
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
       </View>
     );
-  /* @tutinfo */
+
   }
 
   return (
