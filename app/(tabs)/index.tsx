bc77f7c187d12137c707b29a6e4fe220af901618
oskygon <oskygon@yahoo.es>
2024-11-10 11:32:28 -0300

    Se agregan imagenes y botones

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,36 @@
-// import { Link } from "@react-navigation/native"; usamos to en lugar de href
-import { Text, View, StyleSheet, } from "react-native";
+import { View, StyleSheet } from 'react-native';
 
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}>
-
-      <Text style={styles.text}>Hello StickerSmash</Text>
-      
-    
-    
- 
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+     
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
-const styles= StyleSheet.create({
-  container:{
-    backgroundColor: "#25292e",
-    flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-  },
-text:{
-  color:"#fff"
 
-},
-button: {
-  fontSize: 20,
-  textDecorationLine: 'underline',
-  color: '#fff',
-  backgroundColor: "#AAA",
-},
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
